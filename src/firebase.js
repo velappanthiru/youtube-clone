4ecb2c77941258022bb697459fec13d199dead0c
velappanthiru <67815582+velappanthiru@users.noreply.github.com>
2023-08-30 20:02:32 +0530

    minor change in firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,7 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDZrwO_so_coUpQ1mxqGOejGWvxZ-_InOM",
-  authDomain: "clone-v3-6ee9f.firebaseapp.com",
-  projectId: "clone-v3-6ee9f",
-  storageBucket: "clone-v3-6ee9f.appspot.com",
-  messagingSenderId: "496898185231",
-  appId: "1:496898185231:web:e710d88035f60aa4192b7a"
+// === add your firebase config
 };
 
 firebase.initializeApp(firebaseConfig);
